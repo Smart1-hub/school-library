65d34fdda45c0a7b5b61a0d5c2356c078319e639
Ademola Adebayo <scdk@yahoo.com>
2022-08-01 10:42:45 +0100

    Fix linters

--- teacher.rb
+++ teacher.rb
@@ -4,4 +4,8 @@ class Teacher < Person
   def initialize(specialization, name = 'unknown', age, parent_permission: true)
     @specialization = specialization
   end
+
+  def can_use_services?
+    true
+  end
 end
